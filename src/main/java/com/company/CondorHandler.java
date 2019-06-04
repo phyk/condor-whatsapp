dc317439546abbc84332f05505b857c61b84c59e
phyk <ppeter1@smail.uni-koeln.de>
2019-06-04 13:30:51 +0200

    Changed Line Endings

--- src/main/java/com/company/CondorHandler.java
+++ src/main/java/com/company/CondorHandler.java
@@ -1,28 +1,41 @@
 package com.company;
 
 import condor.api.CondorApi;
+import condor.api.contentprocessor.Oscillation;
+import condor.contentprocessor.annotation.turntaking.TimeUnit;
 import condor.data.contract.db.entities.IDataset;
 
 public class CondorHandler {
-    public void connectToCondor()
+    public void connectToCondor(String linkcsv, String actorcsv)
     {
-        CondorApi condor = new CondorApi("jt7455fcff3uiDGFDGACGC5", null);
+        CondorApi condor = new CondorApi("9oi160m0dk8oafa2kkcpgtlbqg", null);
 
+        condor.connectToDataBase("localhost", "3306", "root", "OQhpmmqbKBDvvk6k8EP3", "condorTemp");
+        String datasetname = "myMessages";
 
-
-        condor.connectToDataBase("localhost", "3306", "user", "PASSWORD", "testev");
-        String datasetname = "tree34";
-        for (IDataset d : condor.getAvailableDatasetsFromDatabase()) {
-            System.out.println(d.getName());
-        }
         IDataset iDataset = condor.setOrCreateDataset(datasetname);
-        System.out.println(iDataset.getName());
-        condor.wikiEvolution("Thomas_H._Malone", "ENGLISH", false, null, null, 2, true, "Do not fetch content");
+        condor.importCSV(actorcsv, linkcsv, iDataset, ";");
         condor.openDateBase(iDataset);
-        System.out.println(condor.getNumberOfNodes());
         condor.calcDegree(false);
-        condor.calcReach(2, false);
+        condor.calcBetweenness(false);
+        condor.calcOscillation(Oscillation.MeasureType.BETWEENNESS, "Days", 1, 3, 0.5);
+        condor.calcContribution();
+        condor.calcTurnTaking(0, TimeUnit.DAYS, 4, TimeUnit.DAYS, false, false);
+        // Try it out with "" as language for automatic detection
+        condor.calcSentiment("German", true, "content", null, true, true, false);
+
         condor.calcCommunity();
-        condor.exportActors("actors.csv", "|", null);
+        condor.exportActors("export.csv", ";", null);
+        String fileActor = condor.exportActors(";", null);
+        String file = condor.exportEdges(";");
+        condor.unloadDatasets();
+        condor.deleteDataset(iDataset);
+    }
+
+    public static void main(String[]args)
+    {
+        new CondorHandler().connectToCondor("condor_import.csv", "actors.csv");
+        System.out.println("Done");
+        System.exit(0);
     }
 }
--- src/main/java/com/company/WhatsappDBToCsv.java
+++ src/main/java/com/company/WhatsappDBToCsv.java
@@ -6,19 +6,19 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.io.csv.CsvWriteOptions;
 import tech.tablesaw.io.csv.CsvWriter;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.*;
 import java.util.Date;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class WhatsappDBToCsv {
 
     private Connection conn;
 
-    protected WhatsappDBToCsv(String databaseLocationPath)
+    private WhatsappDBToCsv(String databaseLocationPath)
     {
         try {
             this.conn = openDbConnection(databaseLocationPath);
@@ -32,24 +32,41 @@ public class WhatsappDBToCsv {
         return new WhatsappDBToCsv(databaseLocationPath);
     }
 
-    public static void main(String[] args) throws IOException, SQLException {
-        WhatsappDBToCsv wcs = WhatsappDBToCsv.create(args[0]);
-        wcs.createCSVExport();
-        wcs.closeDbConnection();
-        return;
+    public static void main(String[] args) throws Exception {
+
+        // Add config File with info instead of args
+
+        String message = decrypt12.decrypt(args[1], args[0], "data/msgstore.db");
+
+        if(message.equals("Decryption of crypt12 file was successful.")) {
+            WhatsappDBToCsv wcs = WhatsappDBToCsv.create("data/msgstore.db");
+            wcs.createCSVExport();
+            wcs.closeDbConnection();
+            System.exit(0);
+        }
     }
 
     public void createCSVExport() throws IOException {
-            String sql = "SELECT main.messages.data AS content, " +
-                    "main.messages.key_remote_jid as idRef, " +
-                    "main.messages.key_from_me as myKey, " +
-                    "cast(main.messages.timestamp as text) as timestamp, \n" +
-                    "main.messages.remote_resource as idRem, " +
-                    "main.group_participants.jid as idGroup \n" +
-                    "FROM main.messages LEFT JOIN main.group_participants\n" +
-                    "ON main.messages.key_remote_jid = main.group_participants.gjid\n" +
-                    "WHERE (NOT (idRem = idGroup AND myKey = 0)) AND " +
-                    "(NOT (mykey = 1 AND idRef LIKE '@g.us' AND idGroup = '')) AND NOT idRef LIKE 'status@broadcast'";
+        String sql = " main.messages._id AS linkid," +
+                "main.messages.data AS content, " +
+                "main.messages.key_remote_jid as idRef, " +
+                "main.messages.key_from_me as myKey, " +
+                "cast(main.messages.timestamp as text) as timestamp, \n" +
+                "main.messages.remote_resource as idRem, " +
+                "main.group_participants.jid as idGroup \n" +
+                "FROM main.messages LEFT JOIN main.group_participants\n" +
+                "ON main.messages.key_remote_jid = main.group_participants.gjid\n" +
+                "WHERE (NOT (idRem = idGroup AND myKey = 0)) AND " +
+                "(NOT (mykey = 1 AND idRef LIKE '@g.us' AND idGroup = '')) AND NOT idRef LIKE 'status@broadcast'";
+
+        /*
+         * SQL Fuer iOS
+         * KeyFromMe = ! ZMessageFromMe
+         * content =
+         * group_participants = ZMemberJid
+         * timestamp = ZMessageDate
+         *
+         */
         Table tbl = null;
         try {
             tbl = createTableWithInfo(sql);
@@ -58,28 +75,33 @@ public class WhatsappDBToCsv {
         }
 
         String enddate = getLatestDate();
+        String startdate = getFirstDate();
         int length = tbl.column(0).size();
         String[] values = new String[length];
         Arrays.fill(values, enddate);
         StringColumn etc = StringColumn.create("Endtime", values);
 
         StringColumn stc = StringColumn.create("Starttime", length);
-        tbl.column("timestamp").asStringColumn().mapInto(new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                long time = Long.parseLong(s);
-                return convertSecondsToDate(time);
-            }
+        tbl.column("timestamp").asStringColumn().mapInto(s -> {
+            long time = Long.parseLong(s);
+            return convertSecondsToDate(time);
         }, stc);
 
-        StringColumn content = StringColumn.create("content", length);
-        tbl.column("content").asStringColumn().mapInto(new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                return s.replace("\r\n","").replace(";","").replace("\r","");
-            }
-        }, content);
+        StringColumn id = StringColumn.create("uuid", length);
+        Random r = new Random();
+        tbl.column("linkid").asStringColumn().mapInto(s -> r.nextInt(Integer.MAX_VALUE) + "", id);
 
+        StringColumn content = StringColumn.create("content", length);
+        tbl.column("content").asStringColumn().mapInto(s -> s.replace("\r\n",". ").replace(";","").replace("\r",""), content);
+
+        MessageDigest sha = null;
+            try {
+                sha = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+        }
+
+        HashSet<String> actors = new HashSet<>();
         StringColumn sIdc = StringColumn.create("SourceUuid");
         StringColumn tIdc = StringColumn.create("TargetUuid");
         for (Row row: tbl)
@@ -93,9 +115,9 @@ public class WhatsappDBToCsv {
             if(idGroup.equals(""))
                 idGroup = idMyself;
             else
-                idGroup = idGroup.substring(0,13);
+                idGroup = toHexString(sha.digest(idGroup.substring(0,13).getBytes()));
             if(!idRem.equals(""))
-                idRem = idRem.substring(0,13);
+                idRem = toHexString(sha.digest(idRem.substring(0,13).getBytes()));
             // Group Logic
             if(idRef.endsWith("@g.us"))
             {
@@ -103,17 +125,20 @@ public class WhatsappDBToCsv {
                 {
                     sIdc.append(idMyself);
                     tIdc.append(idGroup);
+                    actors.add(idMyself);
                 }
                 else
                 {
                     sIdc.append(idRem);
                     tIdc.append(idGroup);
+                    actors.add(idRem);
                 }
+                actors.add(idGroup);
             }
             // Chat Logic
             if(idRef.endsWith("@s.whatsapp.net"))
             {
-                idRef = idRef.substring(0,13);
+                idRef = toHexString(sha.digest(idRef.substring(0,13).getBytes()));
                 if(fromMe)
                 {
                     sIdc.append(idMyself);
@@ -124,62 +149,120 @@ public class WhatsappDBToCsv {
                     sIdc.append(idRef);
                     tIdc.append(idMyself);
                 }
+                actors.add(idRef);
+                actors.add(idMyself);
             }
         }
 
-        Table copy = tbl.create(content, sIdc, tIdc, stc, etc);
+        Table copy = Table.create(id, content, sIdc, tIdc, stc, etc);
 
-        CsvWriteOptions.Builder cwo = CsvWriteOptions.builder("condor_import.csv");
+        CsvWriteOptions.Builder cwo = CsvWriteOptions.builder("data/links_import.csv");
         cwo.header(true);
         cwo.separator(';');
 
         CsvWriter cw = new CsvWriter();
         cw.write(copy, cwo.build());
-        return;
+
+
+        StringColumn asc = StringColumn.create("id", new ArrayList<>(actors));
+        StringColumn asc2 = StringColumn.create("uuid", new ArrayList<>(actors));
+        StringColumn asc3 = StringColumn.create("name", new ArrayList<>(actors));
+
+        length = asc.size();
+        values = new String[length];
+        Arrays.fill(values, startdate);
+        StringColumn stc2 = StringColumn.create("starttime", values);
+        values = new String[length];
+        Arrays.fill(values, enddate);
+        StringColumn etc2 = StringColumn.create("endtime", values);
+
+        Table actorTable = Table.create(asc, asc2, asc3, stc2, etc2);
+        cwo = CsvWriteOptions.builder("data/actors_import.csv");
+        cwo.header(true);
+        cwo.separator(';');
+        cw.write(actorTable, cwo.build());
     }
 
+    private static String toHexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
 
-    public String getLatestDate()
+        for (byte aByte : bytes) {
+            String hex = Integer.toHexString(0xFF & aByte);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+
+    private String getLatestDate()
     {
         String selectLatestTimestamp =
                 "SELECT main.messages.timestamp FROM main.messages " +
                 "ORDER BY main.messages.timestamp DESC LIMIT 1";
-        String enddate = null;
-        try(Statement stm = conn.createStatement()) {
-            ResultSet rs = stm.executeQuery(selectLatestTimestamp);
+        String columnLabel = "timestamp";
 
-            long endtime = rs.getLong("timestamp");
-            rs.close();
-            stm.close();
-            enddate = convertSecondsToDate(endtime);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        String enddate = executeSqlAndGetResult(selectLatestTimestamp, columnLabel);
+        if(enddate == null)
+        {
+            enddate = convertSecondsToDate(System.currentTimeMillis());
         }
+
         return enddate;
     }
 
-    public String convertSecondsToDate(long time)
+    private String executeSqlAndGetResult(String sql, String columnLabel)
     {
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+        String enddate;
+        try(Statement stm = conn.createStatement()) {
+            ResultSet rs = stm.executeQuery(sql);
+
+            long endtime = rs.getLong(columnLabel);
+            rs.close();
+            stm.close();
+            enddate = convertSecondsToDate(endtime);
+            return enddate;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getFirstDate() {
+        String selectLatestTimestamp =
+                "SELECT main.messages.timestamp FROM main.messages " +
+                        "ORDER BY main.messages.timestamp ASC LIMIT 1";
+        String columnLabel = "timestamp";
+
+        String enddate = executeSqlAndGetResult(selectLatestTimestamp, columnLabel);
+        if(enddate == null)
+        {
+            enddate = convertSecondsToDate(0);
+        }
+
+        return enddate;
+    }
+
+    private String convertSecondsToDate(long time)
+    {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
         return format.format(new Date(time));
     }
 
-    public Connection openDbConnection(String pathToDb) throws SQLException {
+    private Connection openDbConnection(String pathToDb) throws SQLException {
         String url = "jdbc:sqlite:"+pathToDb;
-        Connection conn = DriverManager.getConnection(url);
-        return conn;
+        return DriverManager.getConnection(url);
     }
 
-    public void closeDbConnection() throws SQLException {
+    private void closeDbConnection() throws SQLException {
         this.conn.close();
     }
 
-    public Table createTableWithInfo(String sql) throws SQLException {
-        Table table = null;
+    private Table createTableWithInfo(String sql) throws SQLException {
         Statement stmt = conn.createStatement();
         ResultSet results = stmt.executeQuery(sql);
-        table = Table.read().db(results);
-
-        return table;
+        return Table.read().db(results);
     }
 }
